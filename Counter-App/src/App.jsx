3d444cac460977f039f2940e73b6af98db6ef91e
sameersiddique987 <sameersiddique301@gmail.com>
2024-08-25 18:37:38 +0500

    counter app

---
+++ Counter-App/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const  App =()=> {
+  const [count , setCount]=useState(0)
+  const ADD = ()=>{
+    setCount(count + 1)
+  }
+
+  const LESS = ()=>{
+    setCount(count - 1)
+  }
+
+  const RESET= ()=>{
+    setCount(0) 
+  }
+  return(
+    <>
+    <h1>COUNTER APP</h1>
+    <h1 >{count}</h1>
+   
+   <button  onClick={ADD}  className="btn btn-info  ">ADD  <i className="fa-solid fa-plus text-light"></i></button>
+   
+   <button onClick={RESET}  className="btn btn-danger m-3  ">RESET </button>
+    <button onClick={LESS}  className="btn btn-warning  ">LESS  <i className=" fa-solid fa-minus text-light"></i></button>
+    </>
+  )
+}
+
+export default App
